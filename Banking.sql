6332351aea67467a144a8afa7df7d131e62015cf
abhijeetsirsat <abhisirsat590@gmail.com>
2026-07-29 09:18:22 +0530

    Created new tables transaction,loans

--- Banking.sql
+++ Banking.sql
@@ -11,3 +11,39 @@ CREATE TABLE Customers (
 );
 describe Customers;
 select * from customers;
+
+CREATE TABLE Accounts (
+	AccountID INT,
+	AccountType VARCHAR (20),
+    Balance DECIMAL(10,2)
+    );
+    
+describe Accounts;
+
+CREATE TABLE Transactions (
+	TransactionID INT,
+    TransactionDate DATE,
+    Amount DECIMAL(10,2),
+    TransactionType VARCHAR(20)
+ );
+ DESCRIBE Transactions;
+ 
+ CREATE TABLE Branches (
+	BranchID INT,
+    BranchName VARCHAR(100),
+    BranchAddress VARCHAR(200),
+    BranchPhone VARCHAR(15)
+);
+describe Branches;
+
+CREATE TABLE AccountBranches (
+	AssignmentDate DATE 
+);
+
+CREATE TABLE Loans (
+	LoanID INT,
+    LoanAmount DECIMAL(10,2),
+    IntrestRate DECIMAL(5,2),
+    StartDate DATE,
+	EndDate DATE
+);
