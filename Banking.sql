585d10561ccb50955119346d1a3c9bf865c78050
abhijeetsirsat <abhisirsat590@gmail.com>
2026-08-04 09:16:32 +0530

    Added data in bankingdb tables

--- Banking.sql
+++ Banking.sql
@@ -162,3 +162,41 @@ desc Loans;
 -- normalization and dinormalization important foe the interwiews
 -- Normalizaton is the process of removing duplicates data from table
 -- divide large table to small table 
+
+INSERT INTO Customers
+(CustomerID, FirstName, LastName, Email, Phone, DateOfBirth)
+VALUES
+(101,'Rahul','Sharma','rahul@gmail.com','9876543210','1998-04-15');
+
+SELECT * FROM Customers;
+
+SELECT * FROM Customers;
+
+INSERT INTO Customers
+VALUES
+(102,'Ketan','Tiwari','ketan@gmail.com','8838938284','2026-08-04','2000-06-22');
+
+SELECT * FROM Customers;
+
+INSERT INTO Customers
+(CustomerID, FirstName, LastName, Email, Phone, AccountCreationDate, DateOfBirth)
+VALUES
+(103,'Neha','Singh','neha@gmail.com','9277476727','2026-08-03','1992-07-03'),
+(104,'Mukul','Jha','mukul@gmail.com','7929267534','2025-02-01','1995-11-06');
+
+-- FOR UPDATE IN COLUMNS
+UPDATE Customers 
+SET 
+    AccountCreationDate = '2025-06-29'
+WHERE
+    CustomerID = '101';
+
+SELECT * FROM Customers;
+
+INSERT INTO Accounts
+(AccountID, CustomerID, AccountType, Balance)
+VALUES 
+(201,101,'Savings',25000);
+
+SELECT * FROM Accounts;
+SELECT * FROM Customers;
